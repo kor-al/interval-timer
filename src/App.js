bfb5d78032c73a10d6e2ded82da43c4380551384
kor-al <korinalice@gmail.com>
2021-04-07 18:31:31 +0300

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
+import './App.css';
+import './Timer.js';
+import React from 'react';
+import ReactFCCtest from 'react-fcctest';
+import Timer from './Timer.js';
+
+
+class Controls extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {count: 25}
+      this.increment = this.increment.bind(this);
+      this.decrement = this.decrement.bind(this);
+  }
+
+  increment(){
+    this.props.update(+1);
+  }
+
+  decrement(){
+    //only allow positive values
+    this.props.update(-1);
+  }
+
+
+  render() {
+      return ( 
+          <div className='controls'>
+            {this.props.count}
+            <button id='increment' onClick={this.increment}>+</button>
+            <button id='decrement' onClick={this.decrement}>-</button>
+          </div>
+      )
+  }
+
+}
+
+
+
+class App extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {count: 25, isTimerPaused: true};
+      this.updateCount = this.updateCount.bind(this);
+      this.updatePause = this.updatePause.bind(this);
+  }
+
+  updateCount(value){
+    if(this.state.count + value >=0 && this.state.isTimerPaused){
+      this.setState({ count: this.state.count + value });
+    }
+    console.log('update');
+  }
+
+  updatePause(value){
+    console.log('pause', value);
+    this.setState({ isTimerPaused: value });
+  }
+  
+  render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Controls type='session' update={this.updateCount}  count={this.state.count}/>
+      <Timer duration ={this.state.count * 60} setPaused={this.updatePause}/>
+      <ReactFCCtest />
     </div>
   );
+    }
 }
 
 export default App;
