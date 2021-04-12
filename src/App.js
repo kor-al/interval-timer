722f79efed45b2c194e8a81d77f2d096564fc77a
kor-al <korinalice@gmail.com>
2021-04-12 17:58:18 +0300

    session and break alternation

--- src/App.js
+++ src/App.js
@@ -15,21 +15,22 @@ class Controls extends React.Component {
   }
 
   increment(){
-    this.props.update(+1);
+    this.props.update(+1, this.props.type);
   }
 
   decrement(){
     //only allow positive values
-    this.props.update(-1);
+    this.props.update(-1, this.props.type);
   }
 
 
   render() {
       return ( 
-          <div className='controls'>
-            {this.props.count}
-            <button id='increment' onClick={this.increment}>+</button>
-            <button id='decrement' onClick={this.decrement}>-</button>
+          <div className='controls' id={`${this.props.type}-controls`}>
+            <div id={`${this.props.type}-label`}>{this.props.type} length</div>
+            <span id={`${this.props.type}-length`}>{this.props.count}</span>
+            <button id={`${this.props.type}-increment`} className="increment" onClick={this.increment}>+</button>
+            <button id={`${this.props.type}-decrement`} className="decrement" onClick={this.decrement}>-</button>
           </div>
       )
   }
@@ -42,41 +43,69 @@ class App extends React.Component {
 
   constructor(props) {
       super(props);
-      this.state = { duration: 25, isTimerPaused: true, minutes: 25, seconds: 0};
+      this.state = this.defaultState();
+
       this.updateDuration = this.updateDuration.bind(this);
+      this.setTypeProperty = this.setTypeProperty.bind(this);
       this.setPause = this.setPause.bind(this);
-      this.setTimeLeft = this.setTimeLeft.bind(this);
-      this.setDuration = this.setDuration.bind(this);
+      this.resetState = this.resetState.bind(this);
   }
 
-  updateDuration(value){
-    if(this.state.duration + value >=0 && this.state.isTimerPaused){
-      this.setDuration(this.state.duration + value);
-      this.setTimeLeft(this.state.duration + value);
+  defaultState(){
+    return {
+      session: { duration: 25, minutes: 25, seconds: 0, default: 25},
+      break: { duration: 5, minutes: 5, seconds: 0, default: 5 },
+      isTimerPaused: true
     }
-    
-    console.log('set start time', this.state.duration, value);
+  }
+
+  resetState() {
+    this.setState(this.defaultState());
+  }
+
+  updateDuration(value, type = 'session'){
+    this.setState((prevState )=>{
+        var typeState = {...prevState [type]};
+        var nextDuration =  typeState.duration + value;
+        console.log('is paused', prevState.isTimerPaused);
+        if( nextDuration > 0 && nextDuration <= 60 && prevState.isTimerPaused){
+          typeState.duration = nextDuration;
+          typeState.minutes = nextDuration;
+          return {[type] : typeState};
+        }
+      })
+    console.log('set start time', this.state[type].duration, value, type);
   }
 
   setPause(value){
-    this.setState({ isTimerPaused: value });
+    console.log('set pause to', value);
+    this.setState({isTimerPaused : value})
   }
   
-  setTimeLeft(min, sec = 0){
-    this.setState({ 'minutes': min, 'seconds': sec })
-  } 
+  // setTimeLeft(min, type, sec = 0){
+  //   var typeState = {...this.state[type]}
+  //   typeState.minutes = value;
+  //   this.setState({typeState})
+  // } 
 
-  setDuration(duration){
-    this.setState({ duration: duration });
+  setTypeProperty(properties , type){
+    //properties = {property: value}
+    var typeState = {...this.state[type]}
+    Object.keys(properties).forEach((property)=>{
+      typeState[property] = properties[property];
+    })
+    this.setState({[type] : typeState})
   }  
   
   
   render(){
     return (
     <div className="App">
-      <Controls type='session' update={this.updateDuration}  count={this.state.duration} />
-      <Timer default={25} duration={this.state.duration*60} minutes={this.state.minutes} seconds={this.state.seconds}
-       setPaused={this.setPause} setTimeLeft={this.setTimeLeft} setDuration={this.setDuration}/> 
+      <Controls type='session' update={this.updateDuration}  count={this.state['session'].duration} />
+      <Controls type='break' update={this.updateDuration}  count={this.state['break'].duration} />
+      <Timer data={this.state} setProperty = {this.setTypeProperty} setPause={this.setPause} reset = {this.resetState}/>
+       {/* duration={this.state.duration*60} minutes={this.state.minutes} seconds={this.state.seconds} */}
+       {/* setPaused={this.setPause} setTimeLeft={this.setTimeLeft} setDuration={this.setDuration}/>  */}
       <ReactFCCtest />
     </div>
   );
--- src/Timer.js
+++ src/Timer.js
@@ -9,6 +9,9 @@ class Timer extends React.Component {
         this.currentDuration = 0; //to track changes in duration on pauses
         this.timerInterval = null
         this.startTime = undefined;
+        this.activeMode = 'session';
+        this.countModeSwitches = 0;
+        this.isPaused = true;
 
         this.update = this.update.bind(this);
         this.clear = this.clear.bind(this);
@@ -16,7 +19,10 @@ class Timer extends React.Component {
         this.pause = this.pause.bind(this);
         this.resume = this.resume.bind(this);
         this.reset = this.reset.bind(this);
-    
+        this.stop = this.stop.bind(this);
+        this.switchMode = this.switchMode.bind(this);
+        this.switchPause = this.switchPause.bind(this);
+
     }
 
     clear() {
@@ -24,13 +30,19 @@ class Timer extends React.Component {
     }
 
     update(timer) {
-        console.log('update state', timer);
         let minutes = parseInt(timer / 60, 10);
         let seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        this.props.setTimeLeft(minutes, seconds)
+        console.log('update state', minutes, seconds);
+
+        if (minutes >= 0 && seconds >= 0) {
+            let properties = {
+                'minutes': minutes,
+                'seconds': seconds
+            }
+            this.props.setProperty(properties, this.activeMode);
+        }
+
     }
 
     setTimerInterval(duration) {
@@ -42,56 +54,80 @@ class Timer extends React.Component {
             this.update(timer);
             if (--timer < 0) {
                 this.clear();
-                this.props.setPaused(true);
+                //this.props.setPause(true);
+                this.switchMode();
+                this.start();
             }
         }, 1000)
     }
 
 
     start() {
-        //if first start
-        if(this.remaining === 0 || (this.currentDuration!==this.props.duration)){
-            this.remaining = this.props.duration;//this.props.minutes*60 + this.props.seconds;
-            this.currentDuration = this.props.duration;
+        if (!this.isPaused) {
+            this.pause();
         }
-        this.resume(this.remaining);
-
+        else {
+            //if first start
+            if (this.remaining === 0 || (this.currentDuration !== this.props.data[this.activeMode].duration)) {
+                this.remaining = this.props.data[this.activeMode].duration * 60 + this.props.data[this.activeMode].seconds;//in seconds;
+                this.currentDuration = this.props.data[this.activeMode].duration;
+            }
+            console.log('remaining', this.remaining);
+            this.resume(this.remaining);
+        }
     }
 
 
     pause() {
-        this.clear()
-        this.props.setPaused(true);
+        this.clear();
+        this.switchPause(true);
+
         let pauseTime = Date.now(); //mseconds
-        this.remaining -= (pauseTime - this.startTime)/1000;
+        this.remaining -= (pauseTime - this.startTime) / 1000;
+    }
+
+    stop() {
+        this.switchPause(true);
+        this.clear();
     }
 
     resume(remaining) {
-        this.props.setPaused(false);
+        this.switchPause(false);
         this.setTimerInterval(remaining);
     }
 
-    reset(){
+    reset() {
         this.clear();
-        this.props.setPaused(true);
-        this.props.setDuration(this.props.default);
-        this.props.setTimeLeft(this.props.default);
+        this.switchPause(true);
         this.remaining = 0;
+        this.props.reset();
     }
 
-    
+    switchMode() {
+        this.activeMode = (this.activeMode === 'session' ? 'break' : 'session');
+        this.remaining = 0;
+        this.countModeSwitches += 1;
+    }
+
+    switchPause(value) {
+        this.isPaused = value;
+        this.props.setPause(this.isPaused);
+    }
+
 
     render() {
-        // let minutes = `${this.props.minutes < 10 ? "0" + this.props.minutes : this.props.minutes}`;
-        // let seconds = `${this.props.seconds < 10 ? "0" + this.props.seconds : this.props.seconds}`;
-        //let minutes = this.props.minutes ;
-        //let seconds = this.props.seconds ;
+
+        let minutes = this.props.data[this.activeMode].minutes;
+        let seconds = this.props.data[this.activeMode].seconds;
+        minutes = `${minutes < 10 ? "0" + minutes : minutes}`;
+        seconds = `${seconds < 10 ? "0" + seconds : seconds}`;
+
         return (
             <div id='timer'>
-                {this.props.minutes} : {this.props.seconds}
-                <button onClick={this.start}>start</button>
-                <button onClick={this.pause}>pause</button>
-                <button onClick={this.reset}>reset</button>
+                <div id="timer-label">{this.activeMode}</div>
+                <div id="time-left">{`${minutes}:${seconds}`}</div>
+                <button id="start_stop" onClick={this.start}>start/pause</button>
+                <button id="reset" onClick={this.reset}>reset</button>
             </div>
         )
     }
